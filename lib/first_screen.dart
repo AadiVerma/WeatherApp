e6017ab62cf4b50912fb4df81ad95c351916566d
git demo <adityakaplish11@gmail.com>
2024-01-02 20:40:05 +0530

    Small Changes

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -25,58 +25,54 @@ class FirstScreen extends StatelessWidget {
             SizedBox(height: 20),
             Skeleton(givenheight: 30.0, givenwidth: 400.0),
             SizedBox(height: 30),
-            Expanded(
-              child: Row(
-                children: [
-                  Skeleton(givenheight: 50.0, givenwidth: 50.0),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0),
-                      SizedBox(height: 10),
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0),
-                    ],
-                  ),
-                  SizedBox(width: 100.0),
-                  Skeleton(givenheight: 50.0, givenwidth: 50.0),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0),
-                      SizedBox(height: 10),
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0)
-                    ],
-                  )
-                ],
-              ),
+            Row(
+              children: [
+                Skeleton(givenheight: 50.0, givenwidth: 50.0),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0),
+                    SizedBox(height: 10),
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0),
+                  ],
+                ),
+                SizedBox(width: 100.0),
+                Skeleton(givenheight: 50.0, givenwidth: 50.0),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0),
+                    SizedBox(height: 10),
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0)
+                  ],
+                )
+              ],
             ),
             SizedBox(height: 20),
             Skeleton(givenheight: 10.0, givenwidth: 400.0),
             SizedBox(height: 20),
-            Expanded(
-              child: Row(
-                children: [
-                  Skeleton(givenheight: 50.0, givenwidth: 50.0),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0),
-                      SizedBox(height: 10),
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0),
-                    ],
-                  ),
-                  SizedBox(width: 100.0),
-                  Skeleton(givenheight: 50.0, givenwidth: 50.0),
-                  SizedBox(width: 10),
-                  Column(
-                    children: [
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0),
-                      SizedBox(height: 10),
-                      Skeleton(givenheight: 20.0, givenwidth: 50.0)
-                    ],
-                  )
-                ],
-              ),
+            Row(
+              children: [
+                Skeleton(givenheight: 50.0, givenwidth: 50.0),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0),
+                    SizedBox(height: 10),
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0),
+                  ],
+                ),
+                SizedBox(width: 100.0),
+                Skeleton(givenheight: 50.0, givenwidth: 50.0),
+                SizedBox(width: 10),
+                Column(
+                  children: [
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0),
+                    SizedBox(height: 10),
+                    Skeleton(givenheight: 20.0, givenwidth: 50.0)
+                  ],
+                )
+              ],
             ),
           ],
         ),
